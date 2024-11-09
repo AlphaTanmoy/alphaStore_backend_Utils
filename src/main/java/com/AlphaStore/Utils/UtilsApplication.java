4a4d76f0f38d52d3af85b4cee199813d0fa16ea5
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-09 21:52:47 +0530

    basefile added

--- src/main/java/com/AlphaStore/Utils/UtilsApplication.java
+++ src/main/java/com/AlphaStore/Utils/UtilsApplication.java
@@ -2,8 +2,10 @@ package com.AlphaStore.Utils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UtilsApplication {
 
 	public static void main(String[] args) {
