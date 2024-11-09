54e89ea8e8247f78dfc0c05dfa41e9b41c1fc8e2
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:31:35 +0530

    update

--- src/main/kotlin/com/AlphaStore/Utils/UtilsApplication.kt
+++ src/main/kotlin/com/AlphaStore/Utils/UtilsApplication.kt
@@ -1,13 +1,11 @@
-package com.AlphaStore.Utils
+package com.alphaStore.Utils
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class UtilsApplication
 
 fun main(args: Array<String>) {
-    runApplication<UtilsApplication>(*args)
+	runApplication<UtilsApplication>(*args)
 }
