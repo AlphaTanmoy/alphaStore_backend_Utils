eda7c6493b0bfe46cbbec6a9390beb0646fb9b8a
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:32:38 +0530

    update

---
+++ src/test/kotlin/com/alphaStore/Utils/UtilsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alphaStore.Utils
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UtilsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
