dfc6cc21e494313ee243ac7ff97145ca5f2e9b67
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 13:27:26 +0530

    bug fix in utils

--- src/main/kotlin/com/AlphaStore/Utils/KeywordsAndConstants.kt
+++ src/main/kotlin/com/AlphaStore/Utils/KeywordsAndConstants.kt
@@ -44,5 +44,14 @@ object KeywordsAndConstants {
     const val TOKEN_REFRESHED_DESCRIPTION = "token is already refreshed"
     const val TOKEN_OR_API_KEY_REQUIRED_DESCRIPTION = "PLease provide either token or api key"
 
+    const val ENCRYPTION_PASSWORD_CHOICE_ONE =
+        "/h9lW;L-~n>l8j\$!IQJHx1Yx{0Ot7:j%W;5b[^8]q}\"G]9jRJgc#P;pDakVkB}G62twX+M6CpscBD;a="
+    const val ENCRYPTION_PASSWORD_CHOICE_TWO =
+        "teA5o?MxI<Mcp+\"RZ|UYK{[>{geCYQdo)bUN57U2p|Ea,lwg4dTK+RBcvird5DdmfakeB[Rdn*wIIhmN"
+    const val ENCRYPTION_PASSWORD_CHOICE_THREE =
+        "/aSM6'A8J,zz9~0)sX*d3pXoo].u<!k+ZThF\\}cRvR7i-\"`X(&'F%4#4c{X8s^/'4Ivh/qKgq;0Q6lz8"
+    const val ENCRYPTION_PASSWORD_SALT_CHOICE = "18237012"
+    const val LENGTH_FOR_ENCRYPTION_KEY = 80
+
 
 }
